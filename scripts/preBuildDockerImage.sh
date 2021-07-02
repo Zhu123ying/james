7870eb2f017692af958f375841ac42e6ec6a9575
lvjin <lvjin@huayun.com>
2021-07-02 17:13:18 +0800

    cicd

--- scripts/preBuildDockerImage.sh
+++ scripts/preBuildDockerImage.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-DOCKER_BUILDPATH=/opt/haihe/docker/front
+DOCKER_BUILDPATH=/opt/haihe/docker/application-front
 
 rm -rf $DOCKER_BUILDPATH
 mkdir -p $DOCKER_BUILDPATH
@@ -9,7 +9,6 @@ npm install && npm run build
 echo "current path: `pwd`"
 
 mv dist $DOCKER_BUILDPATH
-cp deploy/haihe.conf $DOCKER_BUILDPATH/haihe.conf
-cp docker/Dockerfile $DOCKER_BUILDPATH/
+cp docker/* $DOCKER_BUILDPATH/
 
 ls -al $DOCKER_BUILDPATH
